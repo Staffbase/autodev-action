ea85487cce2de98ab0a7047033bb1bc2e23b0c6a
Robert Hecht <robert.hecht@staffbase.com>
2021-10-04 15:50:10 +0200

    pull & checkout

--- autodev.ts
+++ autodev.ts
@@ -17,10 +17,17 @@ const run = async (): Promise<void> => {
     const pulls = allPulls.
         filter(pull => pull.labels.some(l => l.name === "dev"))
 
+    if (pulls.length == 0) {
+        info("nothing to merge.")
+        return
+    }
+
     const branches = pulls.map(pull => pull.head.ref);
     const message = `AutoDev Action Commit\nThe following branches have been merged:\n${branches.map(b => `- ${b}`).join('\n')}`
     await exec('git fetch')
-    await exec('git merge -s octopus origin/dev', [...branches.map(b => `origin/${b}`), "-m", message])
+    await exec('git checkout dev')
+    await exec('git pull')
+    await exec('git merge -s octopus', [...branches.map(b => `origin/${b}`), "-m", message, '--no-ff'])
     
     info(message)
 }
