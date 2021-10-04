94a951aa12c25714d5033b95b9d4c6e7cdeef5ce
Robert Hecht <robert.hecht@staffbase.com>
2021-10-04 14:41:41 +0200

    list prs

--- autodev.ts
+++ autodev.ts
@@ -1,7 +1,19 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 const run = async (): Promise<void> => {
-    core.info("it's working, wowsers")
+    const repoString = process.env['GITHUB_REPOSITORY']
+    if (!repoString) {
+        core.setFailed("couldn't retrieve the repo string. GITHUB_REPOSITORY not set?")
+        return
+    }
+    const [owner, repo] = repoString.split('/')
+    
+    const token = core.getInput('token');
+    const octokit = github.getOctokit(token)
+
+    const {data: pulls} = await octokit.rest.pulls.list({owner, repo})
+    core.info(`it's working, wowsers ${JSON.stringify(pulls, null, 2)}`)
 }
 
 export default run;
