d57731f4a53913d546a66be6168fb7d812de6eec
Uwe Barthel <uwe.barthel@staffbase.com>
2024-07-01 10:52:55 +0200

    refactor: removed externalized plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import {createViteLicensePlugin} from 'rollup-license-plugin'
-import {externalizeDeps} from 'vite-plugin-externalize-deps'
 import {defineConfig} from 'vitest/config'
 
 export default defineConfig({
@@ -7,8 +6,7 @@ export default defineConfig({
     createViteLicensePlugin({
       replenishDefaultLicenseTexts: true,
       outputFilename: 'LICENSES.json'
-    }),
-    externalizeDeps()
+    })
   ],
   build: {
     target: 'esnext',
